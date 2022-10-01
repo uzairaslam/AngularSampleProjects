8fde0e2516763b6951989707fb5f169522e37c6a
uzairaslam <uzairaslam64@gmail.com>
2022-10-02 02:08:03 +0500

    password generator complete

--- pwGenerator/src/app/app.component.ts
+++ pwGenerator/src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwGenerator';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedLength = parseInt(value);
+    if(!isNaN(parsedLength))
+      this.length = parsedLength;
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(){
+    let generatedPassword = '';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()_+';
+    this.password = '';
+
+    if(this.includeLetters)
+      generatedPassword += letters;
+
+    if(this.includeNumbers)
+      generatedPassword += numbers;
+
+    if(this.includeSymbols)
+      generatedPassword += symbols;
+
+    for (let index = 0; index < this.length; index++) {
+      console.log('on loop');
+      let ind = Math.floor(Math.random() * generatedPassword.length);
+      this.password += generatedPassword[ind];
+    }
+  }
 }
